5d2ab741c0367a2b737641e3f357540b10c488f8
Harvy Toralba <60731652+harvytoralba@users.noreply.github.com>
2022-05-24 16:53:45 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,24 @@
 function compute()
 {
-    p = document.getElementById("principal").value;
+    var principal = document.getElementById("principal").value;
+    if(principal > 0){
+        var rate = document.getElementById("rate").value;
+        var years = document.getElementById("years").value;
+        var interest = principal * years * rate / 100;
+        var year = new Date().getFullYear()+parseInt(years);
+        var display = "If you deposit <span class='highlight'>"+principal+"</span>,<br>";
+        display += "at an interest rate of  <span class='highlight'>"+rate+"</span>.<br>";
+        display += "You will receive an amount of  <span class='highlight'>"+interest+"</span>,<br>";
+        display += "in the year  <span class='highlight'>"+year+"</span>";
+        document.getElementById("result").innerHTML = display;
+    }else{
+        alert("Enter a positive number");
+        document.getElementById("principal").focus();
+    }
     
+}
+function updateRate(){
+    var rateval = document.getElementById("rate").value;
+    document.getElementById("rate_val").innerText = rateval+"%";
 }
         
